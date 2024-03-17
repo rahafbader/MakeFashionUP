e5aa20a6398327c49a60b06bf79b5b8992d5a021
rahafbader <Rahaf.bd6@gmail.com>
2024-03-17 03:15:06 +0300

    17march

--- web_project/urls.py
+++ web_project/urls.py
@@ -4,4 +4,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
+     path('login/',include('login.urls')),
+     path('signup/',include('signup.urls')),
 ]
